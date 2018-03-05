c693aee864dbfedf203e29d07e3c749bdb73f231
Shane Thacker <shane@steadymade.com>
2018-03-05 15:37:24 -0600

    Misc refactorings

--- index.js
+++ index.js
@@ -2,63 +2,50 @@ const fs = require('fs');
 const md5File = require('md5-file');
 const walkedFiles = new Map();
 const hashMap = new Map();
-const Path = require('path');
+const { basename, extname } = require('path');
 
-module.exports = function (bundler) {
-  const logger = bundler.logger;
+const packageHasher = mainBundle => walkBundles(mainBundle, '--');
 
-  const packageHasher = (mainBundle) => {
-    walkBundles(mainBundle, '--');
-  };
+const renameReferences = (fileName, hashMap) => {
+  if (fs.existsSync(fileName)) {
+    let data = fs.readFileSync(fileName, 'utf8');
 
-  const walkBundles = (bundle, indent) =>  {
-    for (let child of bundle.childBundles) {
-      if (!walkedFiles.has(child.name)) {
-        walkBundles(child, indent + '--');
-      }
-    }
+    hashMap.forEach((value, key) => {
+      data = data.replace(new RegExp(key, 'g'), value);
+    });
 
-    let type = bundle.entryAsset.type
-    if(type != 'woff' && type != 'woff2') {
-      renameReferences(bundle.name, hashMap);
-    }
+    fs.writeFileSync(fileName, data, 'utf8');
+  } else {
+    console.log(`${fileName} not found. Must have already been moved!`);
+  }
+}
 
-    let oldHash = Path.basename(bundle.name, Path.extname(bundle.name));
-    let hash = md5File.sync(bundle.name);
-    walkedFiles.set(bundle.name, hash);
-    hashMap.set(oldHash, hash);
-    // logger.log(indent + bundle.name)
-    console.log(indent + bundle.entryAsset.name);
-
-    let newLocation = `${bundle.entryAsset.options.outDir}/${hash}.${
-      bundle.entryAsset.type
-    }`;
-    // logger.log(indent + newLocation + " (move here)");
-
-    // If it's not the entry file, rename it
-    if (bundle.parentBundle) {
-      fs.renameSync(bundle.name, newLocation);
+const walkBundles = (bundle, indent) => {
+  for (let child of bundle.childBundles) {
+    if (!walkedFiles.has(child.name)) {
+      walkBundles(child, `${indent}--`);
     }
   }
 
-  const renameReferences = (fileName, hashMap) => {
-    if (fs.existsSync(fileName)) {
-      let data = fs.readFileSync(fileName, 'utf8');
+  const type = bundle.entryAsset.type;
 
-      hashMap.forEach(function(value, key) {
-        data = data.replace(new RegExp(key, 'g'), value);
-      });
-
-      fs.writeFileSync(fileName, data, 'utf8', function(err) {
-        if (err) return console.log(err);
-      });
-    } else {
-      console.log(fileName + ' not found. Must have already been moved!');
-    }
+  if (type != 'woff' && type != 'woff2') {
+    renameReferences(bundle.name, hashMap);
   }
 
-  bundler.on('bundled', (bundle) => {
-    console.log('ABOUT TO FINGERPRINT!!');
-    packageHasher(bundle);
-  });
+  const oldHash = basename(bundle.name, extname(bundle.name));
+  const hash = md5File.sync(bundle.name);
+  const newLocation = `${bundle.entryAsset.options.outDir}/${hash}.${bundle.entryAsset.type}`;
+
+  walkedFiles.set(bundle.name, hash);
+  hashMap.set(oldHash, hash);
+
+  // If it's not the entry file, rename it
+  if (bundle.parentBundle) {
+    fs.renameSync(bundle.name, newLocation);
+  }
+}
+
+module.exports = bundler => {
+  bundler.on('bundled', bundle => packageHasher(bundle));
 }
